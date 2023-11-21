85a3d401a28830543323946612a237aa098ef899
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 12:11:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function highestmarks() {
-  const one = document.querySelector("#stdone").value;
-  const two = document.querySelector("#stdtwo").value;
-  const three = document.querySelector("#stdthree").value;
-  const four = document.querySelector("#stdfour").value;
-  const five = document.querySelector("#stdfive").value;
+function highestmarks(){
+  const one = document.querySelector(#stone").value;
+  const two = document.querySelector("#sttwo").value;
+  const three= document.querySelector("#stthre").value;
+  const four = document.querySelector("#stfour").value;
+  const five = document.querySelector("#stfive").value;
 
-  let marks = [one, two, three, four, five];
-  const highestMarks = Math.max(...marks);
-  let student = marks.indexOf(highestMarks) + 1;
+let marks = [onw,twoo, three, four ,five];
+const  highestmarks= Math.max(...marks);
+  lwt student = marks.indexof(highestmaks)+1;
+  alert(`The highest marks is: {highestmarks}`);
 
-  alert(` The  Highest Mark is: ${highestMarks}`);
-
-  document.querySelector("#result").innerHTML = highestMarks;
+document.querySelector("#result").innerHTML= highestmarks;
 }
