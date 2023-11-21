f1b7ec531ea779161633259f7b98487456198616
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 12:27:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function capitalize(){
+/*function capitalize(){
   const name = document.querySeletor("#name").value;
   const capitalized = name.charAt(0).toUpperCase()+name.slice(1);
 
 document.querySelector("span").innerHTML=capitalized;
+}
+*/
+function capitalize() {
+  //   alert("hello");
+  const name = document.querySelector("#name").value;
+  const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
+
+  //   alert(capitalized);
+  document.querySelector("span").innerHTML = capitalized;
 }
