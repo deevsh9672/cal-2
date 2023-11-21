5d2b395cb026e8f74fa6d201f79a48a811ceef82
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 11:26:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
-// const submit = document.getElementById("submit")
-// submit.addEventListener("click", calculate())
+funcation caculator() {
+let num1 =parseInt(document.getElementById("num1").value);
+let num2 =parseInt(document.getElementById("num2").value);
+let oprator= document..getElementById("oprator")..value);
 
-function calculate() 
-{
-  let num1 = parseInt(document.getElementById("num1").value)
-  let num2 = parseInt(document.getElementById("num2").value);
-  let operator = document.getElementById("operator").value;
-
-  let result;
-  switch (operator)
-   {
-    case "+":
-      result = num1 + num2;
-      break;
-    case "-":
-      result = num1 - num2;
-      break;
-    case "*":
-      result = num1 * num2;
-      break;
-    case "/":
-      result = num1 / num2;
-      break;
-
-    default:
-      result = "Invalid Operator!";
-      break;
-
-  }
-
-  document.getElementById("result").value = result;
+let  result;
+  case "+":
+  result = num1+ num2;
+  break;
+  case "-":
+  result = num1-num2;
+  break;
+  case "*":
+  result= num1*num2;
+  break;
+  case "/":
+  result= num1 / num2;
+  break;
+   defualt:
+   result = "invalid oprator  "
+  break;
 }
 
-
+document.getElementById("result").value= result;
+}
