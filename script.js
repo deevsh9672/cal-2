83f678f681fbff6106472a6d1d44f8abbed6dfd4
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 13:33:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function countvowels(){
+/*function countvowels(){
   let name= docuent.querySelctor("#name").value;
   const vowles = aeiouAEIOU;
 
@@ -11,4 +11,18 @@ count++;
 alert(`the name $ {name} includes $ {count}vowels`);
 
 document.querySeklector("#result").innerHTML=count;
+}
+*/
+
+function countVowels() {
+  let name = document.querySelector("#name").value;
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+  for (let i = 0; i < name.length; i++) {
+    if (vowels.includes(name[i])) {
+      count++;
+    }
+  }
+  alert(`The Name ${name} includes ${count} vowels`);
+  document.querySelector("#result").innerHTML = count;
 }
