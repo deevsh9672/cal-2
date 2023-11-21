4a5359c78c23fcb90ff8867f6d37a0495785b314
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 14:48:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-/*function find()
-{
-  const item1 = document.querySelector("#item1").value;
-  const item2 = document.querySelector("#item2").value;
-  const item3 = document.querySelector("#item3").value;
-  const item4= document.querySelector("#item4").value;
-  const item5 =document.querySelector("#item5").value;
+function convart(){
+const tempinc = document.querySelector("#tempinc").value;
+const tempinf = tempinc*(9/5)+32;
 
-const originalCart = [item1,item2,item3,item4,item5];
-
-const newCart=[...new set(originalCart)];
-document.querySelecotr(".output").innerHTML=newCart;
-}*/
-function find() {
-  const item1 = document.querySelector("#item1").value;
-  const item2 = document.querySelector("#item2").value;
-  const item3 = document.querySelector("#item3").value;
-  const item4 = document.querySelector("#item4").value;
-  const item5 = document.querySelector("#item5").value;
-
-  const originalCart = [item1, item2, item3, item4, item5];
-
-  const newCart = [...new Set(originalCart)];
-  document.querySelector(".output").innerHTML = newCart;
+document.querySelector("span").innerText=tempinf;
 }
-
