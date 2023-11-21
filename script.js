c99785aa3c8047bd7962ad0165aafa5761ac005b
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 13:51:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function find()
+/*function find()
 {
   const item1 = document.querySelector("#item1").value;
   const item2 = document.querySelector("#item2").value;
@@ -10,4 +10,17 @@ const originalCart = [item1,item2,item3,item4,item5];
 
 const newCart=[...new set(originalCart)];
 document.querySelecotr(".output").innerHTML=newCart;
+}*/
+function find() {
+  const item1 = document.querySelector("#item1").value;
+  const item2 = document.querySelector("#item2").value;
+  const item3 = document.querySelector("#item3").value;
+  const item4 = document.querySelector("#item4").value;
+  const item5 = document.querySelector("#item5").value;
+
+  const originalCart = [item1, item2, item3, item4, item5];
+
+  const newCart = [...new Set(originalCart)];
+  document.querySelector(".output").innerHTML = newCart;
 }
+
